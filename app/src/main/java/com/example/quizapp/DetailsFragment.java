3f1b98bd7180954c4a7bc7ed2d38cc2ecc3c3895
Dipali <dipalikharat2798@gmail.com>
2021-06-10 19:09:07 +0530

    first commit

---
+++ app/src/main/java/com/example/quizapp/DetailsFragment.java
@@ -0,0 +1,41 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class DetailsFragment extends Fragment {
+
+    NavController navController;
+    Button btn;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        btn=view.findViewById(R.id.details_start_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_detailsFragment_to_quizFragment);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/quizapp/ListFragment.java
@@ -0,0 +1,76 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quizapp.Adapter.QuizListAdapter;
+import com.example.quizapp.Model.QuizListModel;
+import com.example.quizapp.ViewModel.QuizListViewModel;
+
+import java.util.List;
+
+public class ListFragment extends Fragment implements QuizListAdapter.OnQuizListItemCLicked {
+
+    private RecyclerView listView;
+    private QuizListViewModel quizListViewModel;
+    private NavController navController;
+
+    private QuizListAdapter adapter;
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = view.findViewById(R.id.list_view);
+        adapter = new QuizListAdapter(this);
+
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listView.setHasFixedSize(true);
+        listView.setAdapter(adapter);
+        navController= Navigation.findNavController(view);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        quizListViewModel = new ViewModelProvider(getActivity()).get(QuizListViewModel.class);
+        quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                adapter.setQuizListModels(quizListModels);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        navController.navigate(R.id.action_listFragment_to_detailsFragment);
+    }
+}
